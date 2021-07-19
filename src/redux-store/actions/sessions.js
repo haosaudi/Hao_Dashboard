dc514d8417ae713e9277d84edba62c3b2410ae21
MoizYousuf <moizyousuf24@gmail.com>
2021-07-19 14:59:39 +0500

    set hy

--- src/redux-store/actions/sessions.js
+++ src/redux-store/actions/sessions.js
@@ -1,104 +1,123 @@
-import { POST, GET, DELETE, PUT } from 'src/utils/api_calls'
-import { ActionType } from '.'
-import { toast } from 'react-toastify'
+import { POST, GET, DELETE, PUT } from "src/utils/api_calls";
+import { ActionType } from ".";
+import { toast } from "react-toastify";
 export default class BookingAction {
   static GetAllSessions = (token) => {
     return async (dispatch) => {
-      console.log('get GetAllGroupBooking data in Action', token)
-      dispatch({ type: ActionType.GET_ALL_SESSIONS })
-      await GET('session?limit=50', token).then((data) => {
+      console.log("get GetAllGroupBooking data in Action", token);
+      dispatch({ type: ActionType.GET_ALL_SESSIONS });
+      await GET("session?limit=50", token).then((data) => {
         if (data) {
-          console.log('GOT THE GetAllGroupGROUPBooking ALL!!', data)
-          dispatch({ type: ActionType.GET_ALL_SESSIONS_SUCCESS, payload: data.data })
+          console.log("GOT THE GetAllGroupGROUPBooking ALL!!", data);
+          dispatch({
+            type: ActionType.GET_ALL_SESSIONS_SUCCESS,
+            payload: data.data,
+          });
         } else {
-          dispatch({ type: ActionType.GET_ALL_SESSIONS_FAIL })
+          dispatch({ type: ActionType.GET_ALL_SESSIONS_FAIL });
         }
-      })
-    }
-  }
+      });
+    };
+  };
 
   static GetSessionsById = (id, token) => {
     return async (dispatch) => {
-      console.log('get Sessions by id data in Action=p==', token)
-      dispatch({ type: ActionType.GET_SESSIONS })
+      console.log("get Sessions by id data in Action=p==", token);
+      dispatch({ type: ActionType.GET_SESSIONS });
       await GET(`session/detail/${id}`, token).then((data) => {
         if (data) {
-          console.log('GOT THE GROUPBOOKING ID!!', data)
-          dispatch({ type: ActionType.GET_SESSIONS_SUCCESS, payload: data?.data })
+          console.log("GOT THE GROUPBOOKING ID!!", data);
+          dispatch({
+            type: ActionType.GET_SESSIONS_SUCCESS,
+            payload: data?.data,
+          });
         } else {
-          dispatch({ type: ActionType.GET_SESSIONS_FAIL })
+          dispatch({ type: ActionType.GET_SESSIONS_FAIL });
         }
-      })
-    }
-  }
+      });
+    };
+  };
   static RejectGroupBooking = (id, token, history) => {
     return async (dispatch) => {
       // console.log('update RejectGroupBooking data in Action', data)
-      dispatch({ type: ActionType.GROUPBOOKING_APPROVED })
+      dispatch({ type: ActionType.GROUPBOOKING_APPROVED });
       await POST(`groupBooking/accept/${id}`, null, token).then((data) => {
         if (data) {
-          history.push("/groupbooking")
-          console.log('UPDATE THE BOOKING', data)
-          toast('APPROVED  SUCCESSFULLY')
-          dispatch({ type: ActionType.GROUPBOOKING_APPROVED_SUCCESS })
-          history.push('/bookings')
+          history.push("/groupbooking");
+          console.log("UPDATE THE BOOKING", data);
+          toast("APPROVED  SUCCESSFULLY");
+          dispatch({ type: ActionType.GROUPBOOKING_APPROVED_SUCCESS });
+          history.push("/bookings");
         } else {
-          dispatch({ type: ActionType.GROUPBOOKING_UPDATE_FAIL })
+          dispatch({ type: ActionType.GROUPBOOKING_UPDATE_FAIL });
         }
-      })
-    }
-  }
+      });
+    };
+  };
   static ApprovedGroupBooking = (id, data, token, history) => {
     return async (dispatch) => {
-
       // console.log('update RejectGroupBooking data in Action', data)
-      dispatch({ type: ActionType.GROUPBOOKING_REJECT })
+      dispatch({ type: ActionType.GROUPBOOKING_REJECT });
       await POST(`groupBooking/accept/${id}`, data, token).then((data) => {
         if (data) {
-          console.log('UPDATE THE BOOKING', data)
-          toast('APPROVED  SUCCESSFULLY')
-          history.push('/groupbooking')
+          console.log("UPDATE THE BOOKING", data);
+          toast("APPROVED  SUCCESSFULLY");
+          history.push("/groupbooking");
         } else {
-          dispatch({ type: ActionType.GROUPBOOKING_UPDATE_FAIL })
+          dispatch({ type: ActionType.GROUPBOOKING_UPDATE_FAIL });
         }
-      })
-    }
-  }
+      });
+    };
+  };
   static CreateGroupBooking = (data, token, history) => {
     return async (dispatch) => {
-
       // console.log('update RejectGroupBooking data in Action', data)
-      dispatch({ type: ActionType.GROUPBOOKING_CREATE })
+      dispatch({ type: ActionType.GROUPBOOKING_CREATE });
       await POST(`groupBooking`, data, token).then((data) => {
         if (data) {
-          console.log('UPDATE THE BOOKING', data)
-          toast('CREATED  SUCCESSFULLY')
-          dispatch({ type: ActionType.GROUPBOOKING_CREATE_SUCCESS })
+          console.log("UPDATE THE BOOKING", data);
+          toast("CREATED  SUCCESSFULLY");
+          dispatch({ type: ActionType.GROUPBOOKING_CREATE_SUCCESS });
 
-          history.push('/groupbooking')
+          history.push("/groupbooking");
         } else {
-          dispatch({ type: ActionType.GROUPBOOKING_CREATE_FAIL })
+          dispatch({ type: ActionType.GROUPBOOKING_CREATE_FAIL });
         }
-      })
-    }
-  }
-
+      });
+    };
+  };
 
   static UpdateBooking = (id, data, token, history) => {
     return async (dispatch) => {
-      console.log('update BOOKING data in Action', data)
-      dispatch({ type: ActionType.GROUPBOOKING_UPDATE })
+      console.log("update BOOKING data in Action", data);
+      dispatch({ type: ActionType.GROUPBOOKING_UPDATE });
       await PUT(`booking/${id}`, data, token).then((data) => {
         if (data) {
-          console.log('UPDATE THE GROUPBOOKING', data)
-          toast('GROUPBOOKING UPDATED SUCCESSFULLY')
-          dispatch({ type: ActionType.GROUPBOOKING_UPDATE_SUCCESS })
+          console.log("UPDATE THE GROUPBOOKING", data);
+          toast("GROUPBOOKING UPDATED SUCCESSFULLY");
+          dispatch({ type: ActionType.GROUPBOOKING_UPDATE_SUCCESS });
           // history.push('/gr')
         } else {
-          dispatch({ type: ActionType.GROUPBOOKING_UPDATE_FAIL })
+          dispatch({ type: ActionType.GROUPBOOKING_UPDATE_FAIL });
         }
-      })
-    }
-  }
-
+      });
+    };
+  };
+  static UpdateSession = (id, data, token, history) => {
+    return async (dispatch) => {
+      console.log("update BOOKING data in Action", data);
+      dispatch({ type: ActionType.SESSIONS_UPDATE });
+      await PUT(`session/${id}`, data, token).then((data) => {
+        if (data) {
+          console.log("UPDATE THE SESSION", data);
+          toast("SESSIONS UPDATED SUCCESSFULLY");
+          dispatch({ type: ActionType.SESSIONS_UPDATE_SUCCESS });
+          history.goBack();
+          // history.push('/gr')
+        } else {
+          dispatch({ type: ActionType.SESSIONS_UPDATE_FAIL });
+        }
+      });
+    };
+  };
 }
